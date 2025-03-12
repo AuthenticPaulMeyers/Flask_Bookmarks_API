a5ff3c5ec81111f800e5562657bce553fe2ae482
Paul Meyers <authenticpaulmeyers@gmail.com>
2025-03-12 10:18:52 +0200

    User login setup

--- src/auth.py
+++ src/auth.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
-from src.constants.http_status_code import HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT, HTTP_201_CREATED
+from src.constants.http_status_code import HTTP_201_CREATED, HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT, HTTP_404_NOT_FOUND
 import validators
 from src.database import User, db
+from flask_jwt_extended import create_access_token, create_refresh_token
 
 auth = Blueprint("auth", __name__, url_prefix="/api/v1/auth")
 
+# user register route
 @auth.post("/register")
 def register():
     username = request.json['username']
@@ -21,11 +23,11 @@ def register():
         return jsonify({"Error": "Username is too short"}), HTTP_400_BAD_REQUEST
     
     # validating username without spaces and if is alphanumneric
-    if username.isalnum():
-        return ({"Error": "Username should not be a number and should not have spaces"}), HTTP_400_BAD_REQUEST
+    if not username.isalnum() or " " in username:
+        return jsonify({"Error": "Username should not be alphanumeric"}), HTTP_400_BAD_REQUEST
     
     # validating email
-    if validators.email(email):
+    if not validators.email(email):
         return jsonify({"Error": "Email is invalid"}), HTTP_400_BAD_REQUEST
     
     # check if the user email already exist to satisfy the unique constraint
@@ -46,8 +48,40 @@ def register():
 
     return jsonify({
         'message': "User created",
-        'username': username, 'email': email
+        "user":{
+            'username': username, 'email': email
+        }
         }), HTTP_201_CREATED
+
+# user login route
+@auth.post("/login")
+def login():
+    email = request.json.get("email","")
+    password = request.json.get("password","")
+
+    # get the user from the database using the email
+    user=User.query.filter_by(email=email).first()
+
+    if user:
+        # check password hash
+        is_password_checked = check_password_hash(user.password, password)
+
+        if is_password_checked:
+            refresh=create_refresh_token(identity=user.id)
+            access=create_access_token(identity=user.id)
+
+            return jsonify({
+                'user': {
+                    'refresh': refresh,
+                    'access': access,
+                    'username': user.username,
+                    'email': user.email
+                }}), HTTP_200_OK
+        
+    # if the user is not found
+    return jsonify({"Error": 'Wrong username or password'}), HTTP_404_NOT_FOUND
+
+        
 
 
 @auth.get("/me")
